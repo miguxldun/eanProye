6bc97b943e0697d4975a712f16d3692b54290f35
miguxldun <128773009+miguxldun@users.noreply.github.com>
2025-06-13 14:39:00 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const form = document.querySelector('form')
 const validUsers = ['aleonmo67650', 'geral12']
-const validPass = ['saxhe3-sijCuv-wuhmeh', 'geral12']
+const validPass = ['g8j2Kq-9X2w5R-4r8Yz2', 'geral12']
 form.addEventListener('submit', (e)=>{
     const user = document.querySelector('#user-input')
     const pass = document.querySelector('#pass')
@@ -15,3 +15,4 @@ form.addEventListener('submit', (e)=>{
         console.log('exito')
     }
 })
+
